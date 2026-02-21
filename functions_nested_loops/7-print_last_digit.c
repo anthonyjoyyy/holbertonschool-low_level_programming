f97c05df1df7f7bfdd5e200880601b1767dbc114
anthonyjoyyy <13063@holbertonstudents.com>
2026-02-21 04:45:10 +0000

    last digit-modulo

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_last_digit - prints the last digit of a number
+ * @n: the integer
+ *
+ * Return: the last digit of a number
+ */
+int print_last_digit(int n)
+{
+	int ld;
+
+	ld = n % 10;
+
+	if (ld < 0)
+	{
+		ld = ld * -1;
+	}
+	putchar(ld + '0');
+
+	return (ld);
+}
