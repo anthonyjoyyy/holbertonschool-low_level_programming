f5ebd7cb09b4ff6077e3990d5cb81a85a31916e2
anthonyjoyyy <13063@holbertonstudents.com>
2026-02-21 03:02:27 +0000

    if... else

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_sign - returns the sign of the number
+ * @n: checks the value of the digit
+ *
+ * Return: 1 if greater than zero, 0 if equivalent to 0, -1 if less than 0
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
