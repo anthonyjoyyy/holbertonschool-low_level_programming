3d9f1136501f21fcc11902ced12f2806d08a3c7f
anthonyjoyyy <13063@holbertonstudents.com>
2026-02-24 04:35:05 +0000

    dereferencing

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *reset_to_98 - changes the value on n to 98
+ *@n: value going to be changed
+ *
+ * Return: if succesful, it will be 0
+ */
+
+void reset_to_98(int *n)
+{
+		*n = 98;
+}
