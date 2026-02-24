211023767d9996d54104f71da061fd6fb58ded70
anthonyjoyyy <13063@holbertonstudents.com>
2026-02-24 04:59:52 +0000

    swap

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * swap_int - both integers swap values
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: void
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
