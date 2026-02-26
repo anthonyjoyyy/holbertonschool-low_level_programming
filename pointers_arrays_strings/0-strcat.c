0f1a337eb99bc0f32ff512f7c2da514c66e8d3e9
anthonyjoyyy <13063@holbertonstudents.com>
2026-02-26 04:38:07 +0000

    strcat

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _strcat - joining two strings together
+ * @src: string we're copying from
+ * @dest: where we are pasting the characters
+ *
+ * Return: dest is the destination where we want the string to print
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+	int j = 0;
+	
+	while (dest[i] != '\0')
+	{
+		i++;
+	}	
+
+	while (src[j] != '\0')
+	{
+		j++;
+	}
+
+	dest[i] = '\0';
+
+	return (dest);
+}
