1fc0094a6f0d9265b232adc01b0501bc4b20a4db
anthonyjoyyy <13063@holbertonstudents.com>
2026-02-26 02:51:18 +0000

    betty updates

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -1,25 +1,29 @@
 #include "main.h"
 #include <stdio.h>
-
+/**
+ * print_array - printing the array values
+ * @a: string to be printed
+ * @n: number of characters in the string
+ */
 
 void print_array(int *a, int n)
 {
 	int i;
 
-	if(n <= 0)
+	if (n <= 0)
 	{
 		printf("\n");
 	}
 	for (i = 0; i < n; i++)
 	{
-		
-	if(i < n-1)
-                {
-                        printf("%d, ",a[i]);
-                }
+
+	if (i < n - 1)
+	{
+		printf("%d, ", a[i]);
+	}
 		else
 		{
-			printf("%d\n",a[i]);
+			printf("%d\n", a[i]);
 		}
 	}
 
--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,5 +1,11 @@
 #include "main.h"
-
+/**
+ * _strcpy - copy the string to a new destination
+ * @dest: location we are copying the value to
+ * @src: where we are copying the value from
+ *
+ * Return: the value of destination
+ */
 char *_strcpy(char *dest, char *src)
 {
 	int i = 0;
@@ -7,11 +13,11 @@ char *_strcpy(char *dest, char *src)
 while (src[i] != '\0')
 {
 
-	dest[i]=src[i];
+	dest[i] = src[i];
 
 
 	i++;
 }
-dest[i]='\0';
-		return dest;
+dest[i] = '\0';
+		return (dest);
 }
