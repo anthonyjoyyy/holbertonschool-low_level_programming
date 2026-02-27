cdfeab27b2e74827baf9196a8a924a9fff5c76e8
anthonyjoyyy <13063@holbertonstudents.com>
2026-02-27 04:50:10 +0000

    reverse array

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -9,18 +9,18 @@
 void reverse_array(int *a, int n)
 {
 	int start = 0;
-	int end = n-1;
+	int end = n - 1;
 	int temp;
 
 	while (start < end)
 	{
 		temp = a[start];
 			a[start] = a[end];
-			a[end] = temp;	
+			a[end] = temp;
 
 			start++;
 			end--;
 
 	}
-	
+
 }
