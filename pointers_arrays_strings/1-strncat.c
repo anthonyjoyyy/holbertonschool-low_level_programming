e14ee5e3bcaf0f640baf88b99e960e8dff07f9ca
anthonyjoyyy <13063@holbertonstudents.com>
2026-02-27 02:04:14 +0000

    strncat

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strncat - joining two strings together
+ * @dest: first string
+ * @src: second string
+ *
+ * Return: dest will become the full string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while (j < n && src[j] != '\0')
+	{
+		dest[i] = src[j];
+		j++;
+	}
+dest[i] = '\0';
+
+	return (dest);
+}
